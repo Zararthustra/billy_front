0ae3358648787802174cee19ca58e9cb7b5762a9
Arthur Mayer <arthur.mayer@cap4lab.com>
2022-10-25 11:48:55 +0200

    setup deployment

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,3 +1,4 @@
-export const Host = "http://192.168.1.6:8000/api";
+export const Host = "http://zararthustra.pythonanywhere.com/api";
+// export const Host = "http://192.168.1.6:8000/api";
 // export const Host = "localhost";
 // export const Host = "http://192.168.1.6:3001/";
